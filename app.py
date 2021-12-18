bdc8d837a88d24d2fb7c586ad16e8569e4349d59
RUDRANSH AGNIHOTRI <59211507+rudransh2004@users.noreply.github.com>
2021-12-19 01:24:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_socketio import SocketIO, join_room, leave_room, emit
+from flask_session import Session
+
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'secret'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+Session(app)
+
+socketio = SocketIO(app,logger=True, engineio_logger=True,cors_allowed_origins="*" ,manage_session=False)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if(request.method=='POST'):
+        username = request.json['username']
+        room = request.json['room']
+        #Store the data in session
+        session['username'] = username
+        session['room'] = room
+        return "done"
+@socketio.on('join')
+def join(message):
+    room = session.get('room')
+    join_room(room)
+    emit('status', {'msg':  session.get('username') + ' has entered the room.'}, room=room)
+@socketio.on('text')
+def text(message):
+    room = session.get('room')
+    emit('message', {'msg': session.get('username') + ' : ' + message['msg']}, room=room)
+if __name__ == '__main__':
+    socketio.run(app,debug=False)
