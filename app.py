74edc947ea98a0502bf671573253c41ba014a9bd
RUDRANSH AGNIHOTRI <59211507+rudransh2004@users.noreply.github.com>
2021-12-20 22:47:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,6 @@ def join(message):
 @socketio.on('text')
 def text(message):
     room = session.get('room')
-    emit('message', {'msg': session.get('username') + ' : ' + message['msg']}, room=room)
+    emit('message', {'msg': message['username'] + ' : ' + message['msg']}, room=room)
 if __name__ == '__main__':
     socketio.run(app,debug=False)
